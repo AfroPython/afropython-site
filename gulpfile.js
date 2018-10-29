07b113363d4efb932e3e753dfce963c191b7a31a
Pedro Silva <pedrro2@hotmail.com>
2018-10-29 09:47:50 -0300

    Altera mensagem de commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var ghpages         = require('gh-pages');
 var path            = require('path');
 
 gulp.task('deploy', function (cb) {
-  ghpages.publish(path.join(process.cwd(), 'dist'), cb);
+  ghpages.publish(path.join(process.cwd(), 'dist'), {message: 'Publicado em: ' + new Date()}, cb);
 });
